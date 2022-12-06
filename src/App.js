53da4f55fcd4186049ddb7037e58df8412ea944f
James <jamesnjorio@gmail.com>
2022-12-06 09:18:28 +0300

    Styling navbar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="min-w-full bg-gradient-to-r from-cyan-500 to-blue-500">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+       <Navbar />
       
     </div>
   
