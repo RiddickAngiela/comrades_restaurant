04c1a7e8c7fbc1ce17121e746afa0a4ea15adb1d
James <jamesnjorio@gmail.com>
2022-12-07 08:01:24 +0300

    Added reviews page

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,24 +10,24 @@ function Login() {
   }
 
   return (
-    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mx-8">
-      <div className="mb-4 px-8">
-      <label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+    <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-12 h-96 flex flex-col mx-64">
+      <div className="mb-4 px-8 pt-12">
+      <label className="block text-grey-darker text-sm font-serif font-bold mb-2" for="username">
         Username
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text"></input>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" required></input>
       </div>
       <div className="mb-6 px-8">
-      <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+      <label class="block text-grey-darker text-sm font-serif font-bold mb-2" for="password">
         Password
       </label>
-      <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password"></input>
+      <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" required></input>
       </div>
       <div className="flex items-center justify-between px-8">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button">
         Sign In
       </button>
-      <a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-600" onClick={()=>{ navigate("/register")}}>
+      <a className="inline-block align-baseline font-bold text-sm font-serif hover:text-blue-600" onClick={()=>{ navigate("/register")}}>
         No account? Sign Up
       </a>
       </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,40 +1,46 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom"
 
 function Menu() {
+  let navigate =useNavigate()
+
   return (
-    <div class>
-      <h1>MENU......................</h1>
-      <h1>CHECK OUR TASTY MENU</h1>
-    <table class="border-collapse border border-slate-400 ...">
+    <div className='flex flex-col'>
+      
+       <h1 className='ml-12'>MENU ......................</h1>
+      <h1 className='ml-12 mt-8'>CHECK OUR TASTY MENU</h1>
+
+      <div className='mx-8 mt-12'>
+      <ul className="flex flex-row mx-8">
+        <li className='mx-4'>All</li>
+        <li className='mx-4'>Breakfast</li>
+        <li className='mx-4'>Lunch</li>
+        <li className='mx-4'>Dinner</li>
+      </ul>
+      </div>
+      
+    <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-12 h-full flex flex-col mx-24">
+    <table className="border-collapse border border-slate-400 ...">
   <thead>
     <tr>
-      <th class="border border-slate-300 ...">Image</th>
-      <th class="border border-slate-300 ...">Name</th>
-      <th class="border border-slate-300 ...">Description</th>
-      <th class="border border-slate-300 ...">Price</th>
+      <th className="border border-slate-300">Image</th>
+      <th className="border border-slate-300">Name</th>
+      <th className="border border-slate-300">Description</th>
+      <th className="border border-slate-300">Price (Kshs)</th>
+      <th className="border border-slate-300">Comments</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td class="border border-slate-300 ...">Indiana</td>
-      <td class="border border-slate-300 ...">Indianapolis</td>
-      <td class="border border-slate-300 ...">Indianapolis</td>
-      <td class="border border-slate-300 ...">Indianapolis</td>
-    </tr>
-    <tr>
-      <td class="border border-slate-300 ...">Ohio</td>
-      <td class="border border-slate-300 ...">Columbus</td>
-      <td class="border border-slate-300 ...">Columbus</td>
-      <td class="border border-slate-300 ...">Columbus</td>
-    </tr>
-    <tr>
-      <td class="border border-slate-300 ...">Michigan</td>
-      <td class="border border-slate-300 ...">Detroit</td>
-      <td class="border border-slate-300 ...">Detroit</td>
-      <td class="border border-slate-300 ...">Detroit</td>
+      <td className="border border-slate-300 ">Image</td>
+      <td className="border border-slate-300">Chips Chicken</td>
+      <td className="border border-slate-300 ">Kienyeji chicken + Local Potatoes</td>
+      <td className="border border-slate-300">800</td>
+      <td className="border border-slate-300"><a className="text-blue-600" onClick={()=>{ navigate("/menuitem")}}>Review</a></td>
     </tr>
   </tbody>
 </table>
+</div>
 </div>
   );
 }
