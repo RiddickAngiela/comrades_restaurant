8a555762ae733593959c7ec17a02bdbce07b6761
Riddick <riddick.angiela@student.moringaschool.com>
2022-12-07 07:53:10 +0300

    About us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <div>
-      About us
+      <div className="">
+        <h3>About Us ..</h3>
+        <hr />
+        <p>
+          Meals just just got better with us <br />
+          Introducing Comrades Restaurant
+          <br />
+          The perfect friend for all your meals and dishes
+          <br />
+          Our mission is to help people find and share the perfect meal and dish.
+          <br />
+          Comrades-Restaurant was launched in January 2007.<br></br>Subscribe to our
+          platform and social media sites <br />
+          for new updates so you don't miss
+          <br />
+          your favourite needs.
+        </p>
+      </div>
     </div>
   );
 }
